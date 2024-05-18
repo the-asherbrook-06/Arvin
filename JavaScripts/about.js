761577cdca68091371de215a3994479d87423104
Santhosh. S <2005.santhoshsivakumar@gmail.com>
2024-05-18 11:06:55 +0530

    initial commit

--- JavaScripts/about.js
+++ JavaScripts/about.js
@@ -1,8 +1,10 @@
 let aboutWindowPopout = 0;
+let aboutWindowMinimize = 0;
 
 // About Window
 const aboutOption = document.querySelector('#icon-dropdown .about');
 const closeAboutOption = document.querySelector('#about .action-bar .close');
+const minimizeAboutOption = document.querySelector('#about .action-bar .minimize');
 const aboutWindow = document.getElementById('about');
 
 aboutOption.addEventListener('click', () => {
@@ -29,6 +31,36 @@ document.addEventListener('keydown', function(event) {
 });
 
 closeAboutOption.addEventListener('click', () => {
+    aboutWindow.style.top = '50%';
+    aboutWindow.style.left = "50%";
+    aboutWindow.style.width = "var(--window-width)";
+    aboutWindow.style.height = "var(--window-height)";
+    aboutWindow.style.transform = 'translateX(-50%) translateY(-50%)';
+    document.querySelector('#about .content').style.display = 'grid';
+
     aboutWindow.style.display = 'none';
     aboutWindowPopout = 0;
+    aboutWindowMinimize = 0;
+});
+
+minimizeAboutOption.addEventListener('click', () => {
+    if(aboutWindowMinimize == 0)
+    {
+        aboutWindow.style.top = '95%';
+        aboutWindow.style.left = "1%";
+        aboutWindow.style.width = "15%";
+        aboutWindow.style.transform = 'translateX(0) translateY(0)';
+        document.querySelector('#about .content').style.display = 'none';
+        aboutWindowMinimize = 1;
+    }
+    else
+    {
+        aboutWindow.style.top = '50%';
+        aboutWindow.style.left = "50%";
+        aboutWindow.style.width = "var(--window-width)";
+        aboutWindow.style.height = "var(--window-height)";
+        aboutWindow.style.transform = 'translateX(-50%) translateY(-50%)';
+        document.querySelector('#about .content').style.display = 'grid';
+        aboutWindowMinimize = 0;
+    }
 });
